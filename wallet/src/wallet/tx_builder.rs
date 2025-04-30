a6de55c8c8a6fb817e1d1460737c07c66bc0a1a6
Luis Schwab <luisschwab@protonmail.com>
2025-04-30 15:02:11 -0300

    chore: use `Amount` on tests

--- wallet/src/wallet/tx_builder.rs
+++ wallet/src/wallet/tx_builder.rs
@@ -1096,7 +1096,7 @@ mod test {
             },
         );
 
-        receive_output_in_latest_block(&mut wallet, Amount::ONE_BTC.to_sat());
+        receive_output_in_latest_block(&mut wallet, Amount::ONE_BTC);
 
         // tx1 sending 15k sat to a recipient
         let recip = ScriptBuf::from_hex(
